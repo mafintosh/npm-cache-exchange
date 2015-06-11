8e371c40677ad90316ce128e7f13a337288392ac
Mathias Buus <mathiasbuus@gmail.com>
2015-06-11 11:01:43 -0700

    add --help

--- bin.js
+++ bin.js
@@ -3,6 +3,11 @@
 var exchange = require('./')
 var airpaste = require('airpaste')
 
+if (process.argv.indexOf('--help') > -1) {
+  console.error('Usage: npm-cache-exchange [optional-namespace]')
+  process.exit(0)
+}
+
 var ex = exchange()
 var stream = airpaste('npm-cache-exchange-' + (process.argv[2] || 'any'))
 
